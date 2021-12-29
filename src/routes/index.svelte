0ce1af7da3fbef3040e26a5e83129fce8e213b53
derekjohnsonva@gmail.com <derekjohnsonva@gmail.com>
2021-12-28 19:15:28 -0500

    one red pixel

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,108 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+
+	// https://simeydotme.github.io/svelte-range-slider-pips/
+	import RangeSlider from 'svelte-range-slider-pips';
+	let heat_val = [50];
+	let heat_range = [0, 100];
+	let bouyancy_val = [50];
+	let bouyancy_range = [0, 100];
+	let viscosity_range = [0, 500];
+	let visc_val = [100];
+	let diff_range = [0, 500];
+	let diff_val = [100];
+
+	let fluid_width = 100;
+	let fluid_height = 100;
+
+	let canvas;
+
+	// draw a border around the canvas
+	onMount(() => {
+		const ctx = canvas.getContext('2d');
+		let frame = requestAnimationFrame(loop);
+
+		function loop(t) {
+			frame = requestAnimationFrame(loop);
+
+			const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+			imageData.data[0] = 255;
+			imageData.data[3] = 255;
+
+			ctx.putImageData(imageData, 0, 0);
+		}
+
+		return () => {
+			cancelAnimationFrame(frame);
+		};
+	});
+</script>
+
+<div class="screen">
+	<div class="form">
+		<div class="inputs">
+			<div class="slider">
+				<p>
+					Heat: {heat_val / 100}
+				</p>
+				<RangeSlider bind:values={heat_val} min={heat_range[0]} max={heat_range[1]} />
+			</div>
+			<div class="slider">
+				<p>
+					Bouyancy: {bouyancy_val / 100}
+				</p>
+				<RangeSlider bind:values={bouyancy_val} min={bouyancy_range[0]} max={bouyancy_range[1]} />
+			</div>
+			<div class="slider">
+				<p>
+					Viscosity: {visc_val / 100}
+				</p>
+				<RangeSlider bind:values={visc_val} min={viscosity_range[0]} max={viscosity_range[1]} />
+			</div>
+			<div class="slider">
+				<p>
+					Diffusion: {diff_val / 100}
+				</p>
+				<RangeSlider bind:values={diff_val} min={diff_range[0]} max={diff_range[1]} />
+			</div>
+		</div>
+	</div>
+	<div class="fluid">
+		<div class="center">
+			<canvas bind:this={canvas} height={fluid_height} width={fluid_width} />
+		</div>
+	</div>
+</div>
+
+<style>
+	.screen {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		height: 100vh;
+	}
+	.form {
+		margin: 0 auto;
+		flex: 1;
+		align-items: center;
+		flex-direction: column;
+		justify-content: center;
+	}
+	.inputs {
+		justify-content: center;
+		align-items: center;
+		max-width: 16rem;
+	}
+	.fluid {
+		flex: 1;
+		background-color: black;
+		justify-content: center;
+		align-items: center;
+		display: flex;
+	}
+	.slider {
+		margin: 10px;
+		height: 100%;
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,12 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'app.html',
+		}),
+		ssr: false,
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
